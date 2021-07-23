b33aa6d27396c51d2e56833752fa127d1384ab64
polydome <polydome@outlook.com>
2021-07-23 11:46:08 +0200

    Initial commit

---
+++ blueshare/README.md
@@ -0,0 +1,3 @@
+# blueshare
+
+Sharing Bluetooth audio sinks in a local network
---
+++ blueshare/android/app/src/main/kotlin/com/github/polydome/blueshare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.polydome.blueshare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
