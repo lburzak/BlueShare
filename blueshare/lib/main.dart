79714577650cb234e86bb4e6d091ab4ce23729a9
polydome <polydome@outlook.com>
2021-07-27 18:38:26 +0200

    Add bloc state to layout

--- blueshare/lib/main.dart
+++ blueshare/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:blueshare/bloc/a2dp_bloc.dart';
 import 'package:blueshare/connection_page.dart';
 import 'package:blueshare/devices_page.dart';
+import 'package:blueshare/util/mock.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'bluetooth_device.dart';
 import 'connection_status.dart';
 
 void main() {
@@ -16,21 +18,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BlueShare',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Scaffold(
-          body: ConnectionPage(
-              status: status,
-              device: mockDevice,
-          ),
-        ),
-        '/devices': (context) => Scaffold(
-          body: DevicesPage(
-                status: status,
-                device: mockDevice,
+    return BlocProvider(
+      create: (context) => A2dpBloc(),
+      child: MaterialApp(
+        title: 'BlueShare',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) {
+            final state = context.select((A2dpBloc bloc) => bloc.state);
+            return Scaffold(
+              body: ConnectionPage(
+                status: state.toConnectionStatus(),
+                device: state is A2dpConnected ? state.device : null,
+              ),
+            );
+          },
+          '/devices': (context) {
+            final state = context.select((A2dpBloc bloc) => bloc.state);
+
+            return Scaffold(
+              body: DevicesPage(
+                status: state.toConnectionStatus(),
+                device: state is A2dpConnected ? state.device : null,
                 devicesStream: Stream.value(List.of([
                   mockDevice,
                   mockDeviceTwo,
@@ -40,15 +50,16 @@ class MyApp extends StatelessWidget {
                   mockDevice
                 ])),
               ),
-        )
-      },
-      theme: ThemeData(
-          scaffoldBackgroundColor: const Color(0xff334575),
-          colorScheme: const ColorScheme.dark(
-              background: Color(0xff334575),
-              onBackground: Color.fromARGB(128, 0x45, 0x5d, 0x8b)),
-          canvasColor: Color(0xff334575),
-          textTheme: Typography.whiteMountainView.copyWith(
+            );
+          }
+        },
+        theme: ThemeData(
+            scaffoldBackgroundColor: const Color(0xff334575),
+            colorScheme: const ColorScheme.dark(
+                background: Color(0xff334575),
+                onBackground: Color.fromARGB(128, 0x45, 0x5d, 0x8b)),
+            canvasColor: Color(0xff334575),
+            textTheme: Typography.whiteMountainView.copyWith(
               headline1: Typography.whiteMountainView.headline1!.copyWith(
                   fontWeight: FontWeight.w300,
                   fontSize: 28,
@@ -66,7 +77,22 @@ class MyApp extends StatelessWidget {
                 fontSize: 16,
                 color: Colors.white,
               ),
-          )),
+            )),
+      ),
     );
+  }
+}
+
+extension on A2dpState {
+  ConnectionStatus toConnectionStatus() {
+    if (this is A2dpConnected) {
+      return ConnectionStatus.connected;
+    } else if (this is A2dpConnecting) {
+      return ConnectionStatus.connecting;
+    } else if (this is A2dpDisconnected) {
+      return ConnectionStatus.disconnected;
+    } else {
+      return ConnectionStatus.disconnected;
+    }
   }
 }
