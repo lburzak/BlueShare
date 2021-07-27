518fc5d00b9483c974acf34128c2111b9ea29c16
polydome <polydome@outlook.com>
2021-07-27 18:38:36 +0200

    Add button for toggling state

--- blueshare/lib/connection_page.dart
+++ blueshare/lib/connection_page.dart
@@ -1,10 +1,13 @@
 import 'package:blueshare/bluetooth_device.dart';
 import 'package:blueshare/connection_status.dart';
 import 'package:blueshare/status_indicators.dart';
+import 'package:blueshare/util/mock.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc/a2dp_bloc.dart';
 import 'controls.dart';
 import 'device_info.dart';
 
@@ -12,7 +15,19 @@ class ConnectionPage extends StatelessWidget {
   final ConnectionStatus status;
   final BluetoothDevice? device;
 
-  const ConnectionPage({Key? key, required this.status, this.device}) : super(key: key);
+  const ConnectionPage({Key? key, required this.status, this.device})
+      : super(key: key);
+
+  A2dpEvent nextEvent() {
+    switch (status) {
+      case ConnectionStatus.disconnected:
+        return const A2dpConnectionStarted();
+      case ConnectionStatus.connecting:
+        return const A2dpConnectionEstablished(mockDevice);
+      case ConnectionStatus.connected:
+        return const A2dpConnectionDropped();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +36,12 @@ class ConnectionPage extends StatelessWidget {
         icon: Icons.menu,
         onPressed: () {
           Navigator.pushNamed(context, '/devices');
+        },
+      ),
+      RoundButton(
+        icon: Icons.stream,
+        onPressed: () {
+          context.read<A2dpBloc>().add(nextEvent());
         },
       )
     ];
@@ -44,55 +65,55 @@ class ConnectionPage extends StatelessWidget {
     }
 
     return Padding(
-        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Column(
-            children: [
-              Flexible(
-                flex: 1,
-                child: Center(
-                  child: ConnectionStatusHeader(status: status, style: Theme.of(context).textTheme.headline1),
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Center(
+                child: ConnectionStatusHeader(
+                    status: status,
+                    style: Theme.of(context).textTheme.headline1),
+              ),
+            ),
+            Flexible(
+              flex: 3,
+              child: Center(
+                child: ConnectionStatusIcon(status: status),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Center(
+                child: FractionallySizedBox(
+                  widthFactor: 0.8,
+                  child: status == ConnectionStatus.disconnected
+                      ? Text(
+                          "Connect to a Bluetooth Audio device to start sharing",
+                          style: Theme.of(context).textTheme.headline5,
+                          textAlign: TextAlign.center,
+                        )
+                      : Visibility(
+                          visible: status == ConnectionStatus.connected,
+                          child: DeviceInfo(device: device),
+                        ),
                 ),
               ),
-              Flexible(
-                flex: 3,
-                child: Center(
-                  child: ConnectionStatusIcon(status: status),
+            ),
+            Flexible(
+              flex: 2,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: buttons,
                 ),
               ),
-              Flexible(
-                flex: 1,
-                child: Center(
-                  child: FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: status == ConnectionStatus.disconnected
-                        ? Text(
-                            "Connect to a Bluetooth Audio device to start sharing",
-                            style: Theme.of(context).textTheme.headline5,
-                            textAlign: TextAlign.center,
-                          )
-                        : Visibility(
-                            visible: status == ConnectionStatus.connected,
-                            child: DeviceInfo(
-                              device: device
-                            ),
-                          ),
-                  ),
-                ),
-              ),
-              Flexible(
-                flex: 2,
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: buttons,
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
-      );
+      ),
+    );
   }
 }
